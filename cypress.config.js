5ba0a7e82b72679ef2e0b3911854725f3e4974c3
vish1608 <vishal@ontum.co>
2022-10-14 13:18:48 +0530

    test

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'hy2kdd',
-  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
