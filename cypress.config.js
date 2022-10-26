fe1a21c1fd864fc7d30360a8676c548ab1a7d1b9
vish1608 <vishal@ontum.co>
2022-10-26 12:36:12 +0530

    id

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  // projectId: 'hy2kdd',
+  projectId: 'hy2kdd',
   chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
