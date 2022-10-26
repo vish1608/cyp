9666eff9e9fc8c691292befac66530be19d908df
vish1608 <vishal@ontum.co>
2022-10-26 12:34:09 +0530

    vid

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   // projectId: 'hy2kdd',
   chromeWebSecurity: false,
-  "video": true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
