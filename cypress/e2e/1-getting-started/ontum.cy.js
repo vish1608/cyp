fe516f9da3cf93def905787027b68ef5fc645fab
vish1608 <vishal@ontum.co>
2022-10-28 01:41:50 +0530

    a

--- cypress/e2e/1-getting-started/ontum.cy.js
+++ cypress/e2e/1-getting-started/ontum.cy.js
@@ -2,43 +2,43 @@
 
 import { visit } from "./visit"
 import { signup } from "./signup"
-// import { signin } from "./signin"
-// import { filter } from "./filter"
-// import { sync } from "./sync"
-// import { profile } from "./profile"
-// import { MoreOptions } from "./MoreOptions"
-// // import { home } from "./home"
-// import { chapters } from "./chapters"
-// import { lessons } from "./lessons"
-// import { bookmarks } from "./bookmarks"
-// import { downloads } from "./downloads"
+import { signin } from "./signin"
+import { filter } from "./filter"
+import { sync } from "./sync"
+import { profile } from "./profile"
+import { MoreOptions } from "./MoreOptions"
+// import { home } from "./home"
+import { chapters } from "./chapters"
+import { lessons } from "./lessons"
+import { bookmarks } from "./bookmarks"
+import { downloads } from "./downloads"
 import { help } from "./help"
-// import { cms } from "./cms"
+import { cms } from "./cms"
 
 it('visit', visit)
 
-it('signup', signup)
+// it('signup', signup)
 
 it('signin', signin)
 
-it('filter', filter)
+// it('filter', filter)
 
-it('sync', sync)
+// it('sync', sync)
 
-it('profile', profile)
+// it('profile', profile)
 
-it('MoreOptions', MoreOptions)
+// it('MoreOptions', MoreOptions)
 
-// it('home', home)
+// // it('home', home)
 
-it('chapters', chapters)
+// it('chapters', chapters)
 
-it('lessons', lessons)
+// it('lessons', lessons)
 
-it('bookmarks', bookmarks)
+// it('bookmarks', bookmarks)
 
-it('downloads', downloads)
+// it('downloads', downloads)
 
 it('help', help)
 
-it('cms', cms)
+// it('cms', cms)
