865a09930cd319beaeea51265a927ed49778a65e
vish1608 <vishal@ontum.co>
2022-12-14 16:36:06 +0530

    a

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: '7z2r4f',
   chromeWebSecurity: false,
+  integrationFolder: 'cypress/e2e',
+  experimentalShadowDomSupport: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
