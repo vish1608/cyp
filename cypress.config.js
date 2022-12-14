513b69eb7c7cfe35924ead1bcec9ff45505fcf05
vish1608 <vishal@ontum.co>
2022-12-14 16:43:36 +0530

    fail

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: '7z2r4f',
   chromeWebSecurity: false,
-  integrationFolder: 'cypress/e2e',
   experimentalShadowDomSupport: true,
   e2e: {
     setupNodeEvents(on, config) {
