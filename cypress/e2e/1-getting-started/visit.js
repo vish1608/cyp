224e962f1a28199e9dfd4cb93bfadb4f90ead340
vish1608 <vishal@ontum.co>
2022-12-16 02:27:48 +0530

    z

--- cypress/e2e/1-getting-started/visit.js
+++ cypress/e2e/1-getting-started/visit.js
@@ -4,6 +4,7 @@ const visit = () => {
     indexedDB.deleteDatabase('offline-blobs') //logs out before starting the test
     cy.visit(`${BaseUrl}/login`)
     cy.contains('ssss')
+    cy.visit(`${BaseUrl}/loginn`)
 }
  
 export default {visit}
