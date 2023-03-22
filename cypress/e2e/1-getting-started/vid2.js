d2691fe25ddde009aba8dfbd3dd50c6e6e7f5ba2
vish1608 <vishal@ontum.co>
2023-03-22 15:16:12 +0530

    cc

--- cypress/e2e/1-getting-started/vid2.js
+++ cypress/e2e/1-getting-started/vid2.js
@@ -28,12 +28,47 @@ const vid2 = () => {
                 cy.get('#previous').click();
             }
 
-            cy.get('#video-wrapper', { timeout: 10000 })
+            cy.get('#video-wrapper', { timeout: 5000 }).if('visible')
             .then(($video) => {
                 $video[0].play();
                             
                 // check if video is playing
                 cy.get('video').should('have.prop', 'paused', false).and('have.prop', 'ended', false);
+            })
+            .else()
+            .then(() => {
+
+                // Lesson link
+                cy.url().then(url => {
+                    cy.writeFile('lessons.csv', `${url}` + ',' , { flag: 'a+' }) // paste
+                });
+
+                // Slide number
+                cy.get('.MuiSlider-thumb').then(($page) => {
+                    const pagenum = $page.text()
+                    cy.writeFile('lessons.csv', `${pagenum}` + ',', { flag: 'a+' }) // paste
+                })
+
+                // Lesson Name
+                cy.get('[direction="column"] > .MuiGrid-root > .MuiTypography-root')
+                .then(($temp)=>{
+                    const txt = $temp.text() // copy
+                    cy.writeFile('lessons.csv', `${txt}` + ',' , { flag: 'a+' }) // paste
+                })
+
+                // Lesson details
+                cy.get('.MuiTypography-root').eq(3)
+                .then(($tempp)=>{
+                    const txtt = $tempp.text() // copy
+                    const txttt = txtt.replace(/\|/g, ',')
+                                      .replace(/Curriculum: /g, '')
+                                      .replace(/Subject: /g, '')
+                                      .replace(/Semester: /g, '')
+                                      .replace(/Grade: /g, '')
+                                      .replace(/Language: /g, '');
+                    cy.writeFile('lessons.csv', `${txttt}` + '\n', { flag: 'a+' }) // paste
+                })
+
             })
         })
     }) 
