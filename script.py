f10946adbf74acef5ce4a23560773238ec5e9243
vish1608 <vishal@ontum.co>
2023-05-19 14:58:16 +0530

    cc

--- script.py
+++ script.py
@@ -7,5 +7,5 @@ def replace_text(file_path):
     for line in fileinput.input(file_path, inplace=True):
         print(line.replace("cy.get('.MuiTextField-root > .MuiInputBase-root > .MuiInputBase-input').type('vish@ontum.co')", "cy.get('.MuiTextField-root > .MuiInputBase-root > .MuiInputBase-input').type('vich@ontum.co')"), end="")
 
-# Provide the path to the file you want to modify 
+# Provide the path to the file you want to modify
 replace_text("c/Users/vc168/cyp/cypress/e2e/1-getting-started/signin.js")
